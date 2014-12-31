671a81e542115e65f589befced3e5b2529d58a85
Lorena <lorena.pantano@gmail.com>
2014-12-31 06:15:29 -0500

    add align sample function

--- sqhelper/qc.py
+++ sqhelper/qc.py
@@ -4,7 +4,11 @@ from bcbio.utils import file_exists, safe_makedir
 from bcbio.provenance import do
 from bcbio.distributed.transaction import tx_tmpdir, file_transaction
 from sqhelper import logger
+from sqhelper.group import star_align
+
 import shutil
 
 def quality(data, args):
+    out_prefix = data["sample_id"] + "_"
+    star_align(data, args, data['collapse'], out_prefix)
     return True
